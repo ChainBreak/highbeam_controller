031a30a196e11bbb0b070fdcd2c7c683bd19e302
ChainBreak <thomas.james.rowntree@gmail.com>
2017-11-25 01:09:22 +1030

    Camera now saves something

--- camera_interface.py
+++ camera_interface.py
@@ -6,16 +6,16 @@ import time
 import picamera
 with picamera.PiCamera() as camera:
 	camera.resolution = [320,240]
-    camera.framerate = 30
-
-    stream = io.BytesIO()
+	camera.framerate = 5
+	stream = io.BytesIO()
 	count = 0
-    for foo in camera.capture_continuous(stream, format='png',use_video_port = True):
-
-		with open("img%03i.png"%count,'w') as img:
-			img.write(stream)
-
-        # Truncate the stream to the current position (in case
-        # prior iterations output a longer image)
-        stream.truncate()
-        stream.seek(0)
+	for foo in camera.capture_continuous(stream, format='jpeg',use_video_port = False):
+		stream.truncate()
+		stream.seek(0)
+		filename = "/media/pi/highbeamUSB/img%03i.jpg"%count
+		with open(filename,'wb') as img:
+			img.write(stream.read())
+			#stream.readinto(img)
+	
+		count += 1
+		print(filename)
