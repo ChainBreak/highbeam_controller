de3bde00b823d577323a74755a297b1030b84b81
tom <thomas.james.rowntree@gmail.com>
2017-11-26 09:42:32 +1030

    Rename some things in Unique_file_name

---
+++ unique_file_name.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+
+
+pathCounterDict = {}
+
+def getUniqueFileName(path,nameFormat):
+	startFileName = os.path.join(path,nameFormat % 0)
+	fileName = startFileName
+	if os.path.isdir(path):
+		i = 0
+
+		if startFileName in pathCounterDict:
+			i = pathCounterDict[startFileName]
+
+		fileName = os.path.join(path,nameFormat % i)
+		while os.path.isfile(fileName):
+			i+=1
+			fileName = os.path.join(path,nameFormat % i)
+		pathCounterDict[startFileName] = i
+	return fileName
+
+
+if __name__ == "__main__":
+	for i in range(4):
+		path = getUniqueFileName("/home/tom/pathTest","test%0i.txt")
+		print(path)
+		with open(path,"w") as f:
+			f.write("hello")
+
+	print(pathCounterDict)
